8f0391b1e2ff6d73685b468c151abc50cfdc2c4c
EnriqueAZ <carlos200ah@gmail.com>
2021-04-22 09:39:33 -0500

    Clase del 22 de abril del 2021

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+/* Esta función detecta el navegador 
+
+XMLHttpRequest permite transferir datos del 
+servidor a la página y viceversa*/
 function obtenerAjax(){
     if (window.XMLHttpRequest) {
         xhttp = new XMLHttpRequest();
@@ -13,23 +18,23 @@ var ajax = obtenerAjax();
 por defecto ajax.open("GET","script.php", true);*/
 
 function peticionServidor(){
-    ajax.open("GET","script.php");
+    ajax.open("GET","script.php?" + obtenerQueryString);
     /* ajax.onreadystatechange detecta cualquier cambio en los estados. 
     Si se pone ajax.onreadystatechange(), se ejecuta la funcion. */
 
     /* Simplemente se está asignando a ajax.onreadystatechange ya que 
     el readyState del objeto cambió. */
 
-    /* onreadystatechange determina que gestor de evento será llamado
-    la propiedad readyState del objeto cambie 
+    /* onreadystatechange determina que gestor de evento será llamado 
+    cuando la propiedad readyState del objeto cambie 
     (en este caso respuestaServidor()). */
     ajax.onreadystatechange = respuestaServidor;
     ajax.send(null);
 }
 
-function calcular(){
+/* function calcular(){
     alert("Hola");
-}
+} */
 
 function respuestaServidor() {
     
@@ -42,4 +47,12 @@ function respuestaServidor() {
           } */
         document.getElementById("resultado").innerHTML = ajax.responseText;
     }
+}
+
+function obtenerQueryString(){
+    n1 = encodeURIComponent(document.getElementById("n1").value); 
+    n2 = encodeURIComponent(document.getElementById("n2").value);
+    query = "n1=" + n1 + "&n2=" + n2 + "&nocache=" + Math.random();
+    /* & separa variables */
+    return query;
 }
--- script.php
+++ script.php
@@ -1,6 +1,33 @@
-<?php 
+<!-- <-?php 
     /* $resultado = $_GET["n1"] + $_GET["n2"]; */
-    $resultado = isset($_GET("n1"))  + isset($_GET["n2"]);
-    echo "El resultado es $resultado";
+    /* $resultado = isset($_GET("n1"))  + isset($_GET["n2"]); */
+    echo "El resultado es";
     /* echo "El resultado desde php"; */
+?> -->
+
+<?php 
+    $n1 = $_GET["n1"];
+    $n2 = $_GET["n2"];
+    $resultado = $n1 + $n2;
+    echo "Vacunados de la semana";
 ?>
+<table border="1">
+    <tr>
+        <td>número 1</td>
+        <td>número 2</td>
+        <td>suma</td>
+    </tr>
+    <tr>
+        <td><?= $n1 ?></td>
+        <td><?= $n2 ?></td>
+        <td><?= $resultado ?></td>
+    </tr>
+    <!-- <tr>
+        <td>Martes</td>
+        <td>20</td>
+    </tr>
+    <tr>
+        <td>Miercoles</td>
+        <td>30</td>
+    </tr> -->
+</table>
